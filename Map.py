7ae0be974c84f17d1f07515de531465aa4a0464d
Landesadel <iliillimusicforever@gmail.com>
2024-02-12 18:47:58 +0300

    добавлена генерация леса и рек

--- Map.py
+++ Map.py
@@ -1,13 +1,34 @@
+from utils import get_rand_bool
+from utils import get_rand_cell
+from utils import extension_cell
+
 # 🌲 🌳 🟩 🟦 🚁 🔥 🏥 🏢 🧡 🌩️ 🌧️ 🌥️ 🎖️ 🏆 🧜‍♀️ 🧚 🟫  TODO потом убрать
  
 class Map(object):
     CELL_TYPES = '🟩🌲🟦🏥🏢';
 
-    def generate_rivers():
-        pass
+    def generate_rivers(self, length):
+        river = get_rand_cell(self.width, self.height)
+        point_x, point_y = river[0], river[1]
 
-    def generate_forest():
-        pass
+        self.cells[point_x][point_y] = 2
+
+        while length > 0:
+            next_cell = extension_cell(point_x, point_y)
+            next_x, next_y = next_cell[0], next_cell[1]
+
+            if (self.is_bounds(next_x, next_y)):
+                self.cells[next_x][next_y] = 2 
+                point_x, point_y = next_x, next_y
+
+                length -= 1
+
+
+    def generate_forest(self, chance, range_value):
+        for i in range(self.height):
+            for j in range(self.width):
+                if get_rand_bool(chance, range_value):
+                    self.cells[i][j] = 1
 
     # 0 - поле
     # 1 - дерево
@@ -41,3 +62,9 @@ class Map(object):
             return False
         return True
 
+
+test_map = Map(20, 10)
+test_map.generate_forest(3, 10)
+test_map.generate_rivers(10)
+test_map.generate_rivers(30)
+test_map.get_map()

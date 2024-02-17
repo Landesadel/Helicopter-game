a6af8182ddc7b18f820849e96e3d256026a3a9f6
Landesadel <iliillimusicforever@gmail.com>
2024-02-17 19:54:27 +0300

    добавлена механика и генерация облаков + завершение игры

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from Map import Map
 from Helicopter import Helicopter
 from pynput import keyboard
+from pynput import keyboard
 import time
 import os
 
 TECK_SLEEP = 0.06
 TREE_UPDATE = 50
-FIRE_UPDATE = 100
+CLOUD_UPDATE = 500
+CLOUD_MOVE = 50
+FIRE_UPDATE = 75
 MAP_WIDTH, MAP_HEIGHT = 20, 10
 MOVES = {'w': (-1, 0), 'd': (0, 1), 's': (1, 0), 'a': (0, -1)}
 
 test_map = Map(MAP_WIDTH, MAP_HEIGHT)
-# test_map.get_map()
 
 helico = Helicopter(MAP_WIDTH, MAP_HEIGHT)
-
-from pynput import keyboard
 
 def action_key(key):
     global helico
@@ -26,10 +26,6 @@ def action_key(key):
     if (key in MOVES.keys()):
         point_x, point_y = MOVES[key][0], MOVES[key][1]
         helico.move(point_x, point_y)
-
-    # if key == keyboard.Key.esc:
-    #     # Stop listener
-    #     return False
 
 listener = keyboard.Listener(
     on_press=None,
@@ -52,3 +48,7 @@ while True:
     
     if (tick % FIRE_UPDATE == 0):
         test_map.update_fire(helico)
+
+
+    if (tick % CLOUD_MOVE == 0):
+        test_map.move_clouds(7, 10)
